6bd8f168669dbe572f833d8a9b3747a0d18ea1b7
Saul Baizman <saul.baizman@gmail.com>
2019-11-17 19:09:43 -0500

    Link to an external JavaScript file.
    In this demo, the link is in the <head> section. Usually, the link is just above the closing </body> tag for performance reasons.

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function get_best_os ( ) {
+    var operating_systems = [
+        'windows',      //0
+        'macintosh',    //1
+        'linux',        //2
+        'beOS',         //3
+        'os/2',         //4
+        'unix'          //5
+    ] ;
+
+    return operating_systems[1] ;
+
+}
